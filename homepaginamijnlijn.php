a72726f4a5c4bcca9c07710aba9f056decc5b897
timolee24 <d263568@edu.rocwb.nl>
2020-01-31 09:27:29 +0100

    first commit

---
+++ homepaginamijnlijn.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="stylemijnlijn.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="vertical-menu">
+    <a href="#" class="active">Home</a>
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+    <a href="#">Link 4</a>
+</div>
+
+<ul><li><img src="IMG_20190603_194115.jpg"></li></ul>
+</body>
+</html>
